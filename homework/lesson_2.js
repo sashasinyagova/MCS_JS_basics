09b172d7b0c1c8b790aba6f0cae9af3fde04b587
Sasha Sinyagova <sashasinyagova@gmail.com>
2021-01-01 22:07:03 +0300

    1

---
+++ homework/lesson_2.js
@@ -0,0 +1,7 @@
+let totalCash = +prompt('Your cash')
+let watchesCount = +prompt('How many watches do you want?')
+let earringsCount = +prompt('How many earrings do you want?')
+let price = +prompt('Price of items')
+let watchesSum = watchesCount*price
+let earringsSum = earringsCount*price
+totalCash >= watchesSum+earringsSum
